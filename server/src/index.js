d3f65d5277d66202ff325b7b4bdeaacbba8ea586
wjloo95 <loo.williamj@gmail.com>
2020-04-28 17:17:56 -0400

    initial express server setup

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('hello world'));
+
+const PORT_NUMBER = process.env.PORT || 9000;
+
+app.listen(PORT_NUMBER, () => {
+  console.log(`Listening on port ${PORT_NUMBER}`);
+});

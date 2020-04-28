3bc7e0e9612c23091b94c36355648c671f80857f
wjloo95 <loo.williamj@gmail.com>
2020-04-28 18:02:23 -0400

    add get route for listings

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import { listings } from './mocklistings';
+
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/listings', (req, res) => {
+  res.send(listings);
+});
 
 const PORT_NUMBER = process.env.PORT || 9000;
 

bf633a0eba07a47941fc5d072f4c18466159b0e8
wjloo95 <loo.williamj@gmail.com>
2020-04-28 18:07:14 -0400

    add delete listing route

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,22 @@ import { listings } from './mocklistings';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/listings', (req, res) => {
   res.send(listings);
+});
+
+app.delete('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      const updatedListings = listings.splice(i, 1)[0];
+      return res.send(updatedListings);
+    }
+  }
+  return res.send('Failed to delete listing');
 });
 
 const PORT_NUMBER = process.env.PORT || 9000;

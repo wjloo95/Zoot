240e3cf15e39ad5e141c96ee4c96deb29bfbb80d
wjloo95 <loo.williamj@gmail.com>
2020-04-28 22:29:41 -0400

    create resolvers

---
+++ server/src/graphql/resolvers.ts
@@ -0,0 +1,22 @@
+import { listings } from '../mockListings';
+
+export const resolvers = {
+  Query: {
+    listings: () => {
+      return listings;
+    },
+  },
+  Mutation: {
+    deleteListing: (root: undefined, args: { id: string }) => {
+      const { id } = args;
+
+      for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+          return listings.splice(i, 1)[0];
+        }
+      }
+
+      throw new Error('Failed to delete listing');
+    },
+  },
+};

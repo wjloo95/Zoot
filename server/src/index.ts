fb7750e559730c98d66221e68295559f26c71eba
wjloo95 <loo.williamj@gmail.com>
2020-04-28 22:02:18 -0400

    create apollo express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,13 @@
 import express from 'express';
-
-import { listings } from './mocklistings';
+import { ApolloServer } from 'apollo-server-express';
 
 const app = express();
 
-app.use(express.json());
+const PORT_NUMBER = 9000;
 
-app.get('/listings', (req, res) => {
-  res.send(listings);
-});
+const server = new ApolloServer({});
 
-app.delete('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      const updatedListings = listings.splice(i, 1)[0];
-      return res.send(updatedListings);
-    }
-  }
-  return res.send('Failed to delete listing');
-});
-
-const PORT_NUMBER = process.env.PORT || 9000;
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Listening on port ${PORT_NUMBER}`);

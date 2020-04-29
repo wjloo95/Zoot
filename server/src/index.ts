cfedd06081fd5a429b3c1292ef9f1e84b5ca7008
wjloo95 <loo.williamj@gmail.com>
2020-04-28 22:09:09 -0400

    import schema to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 
 const PORT_NUMBER = 9000;
 
-const server = new ApolloServer({});
+const server = new ApolloServer({ schema });
 
 server.applyMiddleware({ app, path: '/api' });
 

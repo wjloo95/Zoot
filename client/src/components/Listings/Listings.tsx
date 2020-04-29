4ac8447bdb0dcf4ad5b78e66be00a8af82f537cb
wjloo95 <loo.williamj@gmail.com>
2020-04-29 01:37:46 -0400

    add delete listing query

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -1,6 +1,10 @@
 import React, { FunctionComponent } from 'react';
 import { server } from '../../lib/api';
-import { ListingsData } from './types';
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from './types';
 
 const LISTINGS = `
   query Listings {
@@ -18,6 +22,14 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface IProps {}
 
 export const Listings: FunctionComponent<IProps> = () => {
@@ -25,10 +37,25 @@ export const Listings: FunctionComponent<IProps> = () => {
     const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     console.log(data);
   };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: '5d4507a9cf295034813b35c2', // hardcoded id variable,
+      },
+    });
+    console.log(data); // check the console to see the result of the mutation!
+  };
+
   return (
     <>
       <h2>Tiny House Listings</h2>
       <button onClick={fetchListings}>Get our Listings!</button>
+      <button onClick={deleteListing}>Delete a Listing!</button>
     </>
   );
 };

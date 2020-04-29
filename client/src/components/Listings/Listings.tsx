8e5425eb879ba5e75cdc36c13e0897a1dc26a335
wjloo95 <loo.williamj@gmail.com>
2020-04-29 01:17:52 -0400

    add fetchListings function

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -1,7 +1,33 @@
 import React, { FunctionComponent } from 'react';
+import { server } from '../../lib/api';
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 
 interface IProps {}
 
 export const Listings: FunctionComponent<IProps> = () => {
-  return <h2>Tiny House Listings</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch({ query: LISTINGS });
+    console.log(data);
+  };
+  return (
+    <>
+      <h2>Tiny House Listings</h2>
+      <button onClick={fetchListings}>Get our Listings!</button>
+    </>
+  );
 };

b15c34529a4765f867b8bbb6d5e9bb9760281779
wjloo95 <loo.williamj@gmail.com>
2020-04-29 01:28:43 -0400

    create type to pass into fetch call

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { server } from '../../lib/api';
+import { ListingsData } from './types';
 
 const LISTINGS = `
   query Listings {
@@ -21,7 +22,7 @@ interface IProps {}
 
 export const Listings: FunctionComponent<IProps> = () => {
   const fetchListings = async () => {
-    const { data } = await server.fetch({ query: LISTINGS });
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     console.log(data);
   };
   return (

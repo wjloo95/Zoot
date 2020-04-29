6fc3dfae56e2fd56d089ae917f6e233b2b1b7751
wjloo95 <loo.williamj@gmail.com>
2020-04-28 23:32:14 -0400

    type check connectDatabase

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,8 +1,9 @@
 import { MongoClient } from 'mongodb';
+import { Database } from '../lib/types';
 require('dotenv').config();
 
 const url = `mongodb+srv://${process.env.USER}:${process.env.USER_PWD}@${process.env.CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`;
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,

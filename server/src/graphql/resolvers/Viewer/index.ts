c67e4017f2bbf091311eb98bb501596d9cfe609c
wjloo95 <loo.williamj@gmail.com>
2020-04-30 15:53:17 -0400

    create authURL query resolver

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,13 +1,19 @@
 import { IResolvers } from 'apollo-server-express';
+import { Google } from '../../../lib/api';
+import { LogInArgs } from './types';
 
 export const viewerResolvers: IResolvers = {
   Query: {
     authUrl: () => {
-      return 'Query.authUrl';
+      try {
+        return Google.authUrl;
+      } catch (error) {
+        throw new Error(`Failed to query Google Auth URL: ${error}`);
+      }
     },
   },
   Mutation: {
-    logIn: () => {
+    logIn: (type: LogInArgs) => {
       return 'Mutation.logIn';
     },
     logOut: () => {

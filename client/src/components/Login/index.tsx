2c37a4162ad31f05fe0dbcc4001c3503ceee6875
wjloo95 <loo.williamj@gmail.com>
2020-04-30 18:21:42 -0400

    add redirect to google button

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Card, Layout, Typography } from 'antd';
-import './Login.css';
+import { useApolloClient } from '@apollo/react-hooks';
 
 // Image Assets
 import googleLogo from './assets/google_logo.jpg';
 import facebookLogo from './assets/f_logo_RGB-White_58.png';
-import { Viewer } from '../../lib/types';
+import './Login.css';
 
+import { Viewer } from '../../lib/types';
+import { AuthUrl as AuthUrlData } from '../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl';
+import { AUTH_URL } from '../../lib/graphql/queries/AuthUrl';
+
+import { Card, Layout, Typography } from 'antd';
 const { Content } = Layout;
 const { Text, Title } = Typography;
-
 interface IProps {
   setViewer: (viewer: Viewer) => void;
 }
 
 export const Login = ({ setViewer }: IProps) => {
+  const client = useApolloClient();
+
+  const handleAuthorize = async () => {
+    try {
+      const { data } = await client.query<AuthUrlData>({
+        query: AUTH_URL,
+      });
+      window.location.href = data.authUrl;
+    } catch {}
+  };
+
   return (
     <Content className="log-in">
       <Card className="log-in-card">
@@ -39,7 +53,10 @@ export const Login = ({ setViewer }: IProps) => {
             Continue with Facebook
           </span>
         </button>
-        <button className="log-in-card-button log-in-card__google-button">
+        <button
+          className="log-in-card-button log-in-card__google-button"
+          onClick={handleAuthorize}
+        >
           <img
             src={googleLogo}
             alt="Google Logo"

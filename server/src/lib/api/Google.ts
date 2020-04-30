71e4dda9004e9dc6f2f43318c2259b435aac56ae
wjloo95 <loo.williamj@gmail.com>
2020-04-30 15:27:36 -0400

    create auth url

---
+++ server/src/lib/api/Google.ts
@@ -0,0 +1,18 @@
+import { google } from 'googleapis';
+
+const authClient = new google.auth.OAuth2(
+  process.env.G_CLIENT_ID,
+  process.env.G_CLIENT_SECRET,
+  `${process.env.PUBLIC_URL}/login`
+);
+
+export const Google = {
+  authUrl: authClient.generateAuthUrl({
+    access_type: 'online',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  }),
+  logIn: async (code: string) => {},
+};

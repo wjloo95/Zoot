b1286ac0c6bb38208a8ad62ce07c730dd16e02a9
wjloo95 <loo.williamj@gmail.com>
2020-04-29 22:48:41 -0400

    create type structure for three collections

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,23 +1,56 @@
 import { ObjectID, Collection } from 'mongodb';
 
+export enum ListingType {
+  Apartment = 'APARTMENT',
+  House = 'HOUSE',
+}
+
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
 export interface Listing {
   _id: ObjectID;
   title: string;
+  description: string;
   image: string;
+  host: string;
+  admin: string;
+  type: ListingType;
   address: string;
+  city: string;
+  country: string;
+  bookings: ObjectID[];
+  bookingsIndex: BookingsIndex;
   price: number;
   numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
 }
 
 export interface User {
-  _id: ObjectID;
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectID[];
+  listings: ObjectID[];
 }
 
 export interface Booking {
   _id: ObjectID;
+  listing: ObjectID;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {

af637004f97a951dcb9a1cae9e365525c5b1a7f1
wjloo95 <loo.williamj@gmail.com>
2020-04-30 23:00:43 -0400

    create user listings bookings typedefs

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -9,12 +9,63 @@ export const typeDefs = gql`
     didRequest: Boolean!
   }
 
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
+
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
+
+  enum ListingType {
+    APARTMENT
+    HOUSE
+    HOTEL
+  }
+
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+
+  type Listings {
+    total: Int!
+    result: [Listing!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+
   input LogInInput {
     code: String!
   }
 
   type Query {
     authUrl: String!
+    user(id: ID!): User!
   }
 
   type Mutation {

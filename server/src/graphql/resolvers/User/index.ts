611af731fc45ffcf1af10a17166bab2002673621
wjloo95 <loo.williamj@gmail.com>
2020-04-30 23:54:40 -0400

    check authorized on user resolver

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,19 +1,27 @@
 import { IResolvers } from 'apollo-server-express';
 import { UserArgs } from './types';
 import { Database, User } from '../../../lib/types';
+import { Request } from 'express';
+import { authorize } from '../../../lib/utils';
 
 export const userResolvers: IResolvers = {
   Query: {
     user: async (
       _root: undefined,
       { id }: UserArgs,
-      { db }: { db: Database }
+      { db, req }: { db: Database; req: Request }
     ): Promise<User> => {
       try {
         const user = await db.users.findOne({ _id: id });
 
         if (!user) {
           throw new Error("User can't be found");
+        }
+
+        const viewer = await authorize(db, req);
+
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true;
         }
         return user;
       } catch (error) {

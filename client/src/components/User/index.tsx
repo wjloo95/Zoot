9e302608c9207ffcf2becbdcddb021e240d54f50
wjloo95 <loo.williamj@gmail.com>
2020-05-01 13:57:56 -0400

    add user profile card

--- client/src/components/User/index.tsx
+++ client/src/components/User/index.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Col, Layout, Row } from 'antd';
+
+import { USER } from '../../lib/graphql/queries';
+import {
+  User as UserData,
+  UserVariables,
+} from '../../lib/graphql/queries/User/__generated__/User';
+import { useParams } from 'react-router-dom';
+import { UserProfile } from './children';
+
+const { Content } = Layout;
 
 export const User = () => {
-  return <h2>User</h2>;
+  const { id } = useParams();
+
+  const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
+    variables: {
+      id,
+    },
+  });
+
+  const user = data ? data.user : null;
+  const userProfileElement = user ? <UserProfile user={user} /> : null;
+
+  return (
+    <Content className="user">
+      <Row gutter={12} justify="start">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+  );
 };

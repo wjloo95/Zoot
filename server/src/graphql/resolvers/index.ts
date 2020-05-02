79df4aa1c4a1c96b7afa194408d54eb73d84b3eb
wjloo95 <loo.williamj@gmail.com>
2020-05-02 16:10:13 -0400

    import listings resolver

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -3,8 +3,10 @@ import { listingResolvers } from './Listing';
 import { userResolvers } from './User';
 import { viewerResolvers } from './Viewer';
 import { bookingResolvers } from './Booking';
+import { listingsResolvers } from './Listings';
 
 export const resolvers = merge(
+  listingsResolvers,
   listingResolvers,
   bookingResolvers,
   userResolvers,

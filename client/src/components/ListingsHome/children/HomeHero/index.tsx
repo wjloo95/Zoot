ef182c75ad07c9cbce91a9f7766781dad5bb5687
wjloo95 <loo.williamj@gmail.com>
2020-05-02 15:23:03 -0400

    add basic search

--- client/src/components/ListingsHome/children/HomeHero/index.tsx
+++ client/src/components/ListingsHome/children/HomeHero/index.tsx
@@ -10,7 +10,11 @@ import londonImage from '../../assets/london.jpg';
 const { Title } = Typography;
 const { Search } = Input;
 
-export const HomeHero = () => {
+interface IProps {
+  onSearch: (value: string) => void;
+}
+
+export const HomeHero = ({ onSearch }: IProps) => {
   return (
     <div className="home-hero">
       <div className="home-hero__search">
@@ -22,6 +26,7 @@ export const HomeHero = () => {
           size="large"
           enterButton
           className="home-hero__search-input"
+          onSearch={onSearch}
         />
       </div>
       <Row gutter={12} className="home-hero__cards">
--- client/src/components/ListingsHome/index.tsx
+++ client/src/components/ListingsHome/index.tsx
@@ -4,16 +4,22 @@ import { Layout } from 'antd';
 import { HomeHero } from './children';
 
 import mapBackground from './assets/map-background.jpg';
+import { useHistory } from 'react-router-dom';
 
 const { Content } = Layout;
 
 export const ListingsHome = () => {
+  let history = useHistory();
+
+  const onSearch = (value: string) => {
+    history.push(`/listings/${value}`);
+  };
   return (
     <Content
       className="home"
       style={{ backgroundImage: `url(${mapBackground})` }}
     >
-      <HomeHero />
+      <HomeHero onSearch={onSearch} />
     </Content>
   );
 };

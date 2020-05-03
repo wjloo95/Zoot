9148ce3832ddfdba8555f1e125a513d22127a33e
wjloo95 <loo.williamj@gmail.com>
2020-05-03 17:32:02 -0400

    add basic form

--- client/src/components/Host/index.tsx
+++ client/src/components/Host/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Form, Layout, Typography } from 'antd';
+import { Form, Layout, Typography, Input, InputNumber, Radio } from 'antd';
 import { Viewer } from '../../lib/types';
+import { ListingType } from '../../lib/graphql/globalTypes';
+import { HomeOutlined, BankOutlined, TeamOutlined } from '@ant-design/icons';
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
@@ -44,6 +46,62 @@ export const Host = ({ viewer }: IProps) => {
             about your listing.
           </Text>
         </div>
+
+        <Item label="Home Type">
+          <Radio.Group>
+            <Radio.Button value={ListingType.APARTMENT}>
+              <BankOutlined />
+              <span>Apartment</span>
+            </Radio.Button>
+            <Radio.Button value={ListingType.HOUSE}>
+              <HomeOutlined />
+              <span>House</span>
+            </Radio.Button>
+            <Radio.Button value={ListingType.HOTEL}>
+              <TeamOutlined />
+              <span>Hotel</span>
+            </Radio.Button>
+          </Radio.Group>
+        </Item>
+
+        <Item label="Title" extra="Max character count of 45">
+          <Input
+            maxLength={45}
+            placeholder="The iconic and luxurious Bel-Air mansion"
+          />
+        </Item>
+
+        <Item label="Description of listing" extra="Max character count of 400">
+          <Input.TextArea
+            rows={3}
+            autoSize
+            maxLength={400}
+            placeholder={`
+              Modern, clean, and iconic home of the Fresh Prince.
+              Situated in the heart of Bel-Air, Los Angeles.
+            `}
+          />
+        </Item>
+
+        <Item label="Address">
+          <Input placeholder="251 North Bristol Avenue" />
+        </Item>
+
+        <Item label="City/Town">
+          <Input placeholder="Los Angeles" />
+        </Item>
+
+        <Item label="State/Province">
+          <Input placeholder="California" />
+        </Item>
+
+        <Item label="Zip/Postal Code">
+          <Input placeholder="Please enter a zip code for your listing!" />
+        </Item>
+
+        <Item label="Price" extra="All prices in $USD/day">
+          <InputNumber min={0} placeholder="120" />
+        </Item>
       </Form>
     </Content>
   );

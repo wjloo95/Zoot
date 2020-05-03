00fc586a93683bda9ce2d608c6cf29a4743b0a53
wjloo95 <loo.williamj@gmail.com>
2020-05-03 01:29:31 -0400

    add second row for loading

--- client/src/components/Listings/children/ListingsSkeleton/index.tsx
+++ client/src/components/Listings/children/ListingsSkeleton/index.tsx
@@ -32,6 +32,29 @@ export const ListingsSkeleton = () => {
           </List.Item>
         )}
       />
+      <List
+        grid={{
+          gutter: 8,
+          xs: 1,
+          sm: 2,
+          lg: 4,
+        }}
+        dataSource={emptyData}
+        renderItem={() => (
+          <List.Item>
+            <Card
+              cover={
+                <div
+                  style={{ backgroundImage: `url(${listingLoadingCardCover})` }}
+                  className="listings-skeleton__card-cover-img"
+                ></div>
+              }
+              loading
+              className="listings-skeleton__card"
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 };

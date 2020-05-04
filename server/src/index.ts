75c1b3c13aca7c151f5a7b8182305b4e06efbe8f
wjloo95 <loo.williamj@gmail.com>
2020-05-04 01:20:14 -0400

    up max input size

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 import express, { Application } from 'express';
+import bodyParser from 'body-parser';
 import { ApolloServer } from 'apollo-server-express';
 import { connectDatabase } from './db';
 import { typeDefs, resolvers } from './graphql';
@@ -11,6 +12,7 @@ const port = process.env.PORT || 9000;
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
+  app.use(bodyParser.json({ limit: '2mb' }));
   app.use(cookieParser(process.env.SECRET));
 
   const server = new ApolloServer({

83e55097b719cde3609447864fce08e650095012
wjloo95 <loo.williamj@gmail.com>
2020-05-04 22:05:06 -0400

    add regex to search

--- client/src/lib/utils/searchValidate/index.ts
+++ client/src/lib/utils/searchValidate/index.ts
@@ -1,6 +1,5 @@
 export const searchValid = (value: string) => {
   const trimmedValue = value.trim();
-  // const onlyChars = /^[a-zA-Z]+$/;
-  //  !onlyChars.test(trimmedValue);
-  return trimmedValue === '';
+  const onlyChars = /^([a-zA-Z0-9]+\s)*[a-zA-Z0-9]+$/;
+  return trimmedValue === '' || !onlyChars.test(trimmedValue);
 };

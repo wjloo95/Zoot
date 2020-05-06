2dec21d026613f7892511545173e6a8c2ac24184
wjloo95 <loo.williamj@gmail.com>
2020-05-06 03:21:52 -0400

    add premium databases

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -66,6 +66,8 @@ export interface Booking {
 export interface Database {
   listings: Collection<Listing>;
   users: Collection<User>;
+  premium_listings: Collection<Listing>;
+  premium_users: Collection<User>;
   bookings: Collection<Booking>;
 }
 

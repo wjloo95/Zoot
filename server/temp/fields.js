7555f3eece4fc434e01fe4462103d1798a1e5e68
wjloo95 <loo.williamj@gmail.com>
2020-05-06 03:07:32 -0400

    complete ETL script

---
+++ server/temp/fields.js
@@ -0,0 +1,36 @@
+const extractFields = [
+  'Name',
+  'Description',
+  'Notes',
+  'House Rules',
+  'Thumbnail Url',
+  'Picture Url',
+  'Street',
+  'City',
+  'State',
+  'Country',
+  'Property Type',
+  'Room Type',
+  'Cancellation Policy',
+];
+
+const hostFields = [
+  'Host ID',
+  'Host Name',
+  'Host Since',
+  'Host Location',
+  'Host About',
+  'Host Thumbnail Url',
+];
+
+const numExtractFields = [
+  'Latitude',
+  'Longitude',
+  'Accommodates',
+  'Bathrooms',
+  'Bedrooms',
+  'Price',
+  'Minimum Nights',
+  'Number of Reviews',
+  'Review Scores Rating',
+];

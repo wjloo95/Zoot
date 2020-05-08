1b46f8b96d759a6ef4c900a1e3dcb6845e839f25
wjloo95 <loo.williamj@gmail.com>
2020-05-08 01:24:52 -0400

    fix header skeleton

--- client/src/lib/components/AppHeaderSkeleton/index.tsx
+++ client/src/lib/components/AppHeaderSkeleton/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Avatar } from 'antd';
+import logo2 from '../../../components/AppHeader/assets/Logo2.png';
 
 const { Header } = Layout;
 
 export const AppHeaderSkeleton = () => {
   return (
     <Header className="app-header">
-      <div className="app-header__logo-search-section">
-        <div className="app-header__logo">Hi!</div>
+      <div className="app-header-search-section">
+        <div className="app-header-logo">
+          <Avatar
+            src={logo2}
+            shape="square"
+            style={{ width: '90px', height: '100%', verticalAlign: 'top' }}
+          />
+        </div>
       </div>
     </Header>
   );

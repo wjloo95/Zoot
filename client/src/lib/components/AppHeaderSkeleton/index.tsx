ef62ccc55880baf68f2c54989757bde3e43acb2f
wjloo95 <loo.williamj@gmail.com>
2020-05-09 18:09:41 -0400

    update header skeleton

--- client/src/lib/components/AppHeaderSkeleton/index.tsx
+++ client/src/lib/components/AppHeaderSkeleton/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Layout, Avatar } from 'antd';
-import logo2 from '../../../components/AppHeader/assets/NewLogoCropped.png';
-
-const { Header } = Layout;
+import Logo from '../../../components/AppHeader/assets/NewLogoCropped.png';
 
 export const AppHeaderSkeleton = () => {
   return (
-    <Header className="app-header">
+    <div className="app-header">
       <div className="app-header-search-section">
         <div className="app-header-logo">
-          <Avatar
-            src={logo2}
-            shape="square"
-            style={{ width: '90px', height: '100%', verticalAlign: 'top' }}
-          />
+          <img src={Logo} alt="Zoot" />
         </div>
       </div>
-    </Header>
+    </div>
   );
 };

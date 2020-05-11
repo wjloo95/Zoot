31ce7a88a4559eb0d081078150cd4815892d3415
wjloo95 <loo.williamj@gmail.com>
2020-05-11 00:30:46 -0400

    show active page on homepage

--- client/src/components/ListingsHome/children/HomeHero/index.tsx
+++ client/src/components/ListingsHome/children/HomeHero/index.tsx
@@ -3,22 +3,33 @@ import React from 'react';
 import BookingsVideo from '../../assets/MainVideo.mp4';
 import Logo from '../../../../lib/assets/LightLogo.png';
 import { LandingSearchBar } from '../../../LandingSearchBar';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export const HomeHero = () => {
+  const history = useHistory();
+
+  const flightStyles =
+    history.location.pathname === '/flights' ? ' listings-header-active' : '';
+  const listingStyles =
+    history.location.pathname === '/listings' ? ' listings-header-active' : '';
+  const experienceStyles =
+    history.location.pathname === '/experiences'
+      ? ' listings-header-active'
+      : '';
+
   return (
     <div className="listings-video-container">
       <div className="listings-header">
         <img src={Logo} alt="Zoot" />
         <div className="listings-header-nav">
-          <Link to="/flights">
-            <div className="listings-flights">Flights</div>
+          <Link to="/flights" className={flightStyles}>
+            <div>Flights</div>
           </Link>
-          <Link to="/listings">
-            <div className="listings-stays">Stays</div>
+          <Link to="/listings" className={listingStyles}>
+            <div>Stays</div>
           </Link>
-          <Link to="/experiences">
-            <div className="listings-experiences">Experiences</div>
+          <Link to="/experiences" className={experienceStyles}>
+            <div>Experiences</div>
           </Link>
         </div>
       </div>

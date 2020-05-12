83f7c4bb4af24d5232e2d9239f3101279f345b69
wjloo95 <loo.williamj@gmail.com>
2020-05-12 04:57:30 -0400

    add favoriteListings on Google login

--- server/src/graphql/resolvers/Viewer/logInViaGoogle.ts
+++ server/src/graphql/resolvers/Viewer/logInViaGoogle.ts
@@ -62,6 +62,7 @@ export const logInViaGoogle = async (
         location: 'No Location Provided',
         bookings: [],
         listings: [],
+        favoriteListings: [],
         token,
       },
     },

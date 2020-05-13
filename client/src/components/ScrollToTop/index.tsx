66f19871d95534d10a6a983881999b89149a949d
wjloo95 <loo.williamj@gmail.com>
2020-05-13 01:34:45 -0400

    scroll to top on route change

--- client/src/components/ScrollToTop/index.tsx
+++ client/src/components/ScrollToTop/index.tsx
@@ -6,14 +6,12 @@ export const ScrollToTop = () => {
 
   useEffect(() => {
     try {
-      // trying to use new API - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
       window.scroll({
         top: 0,
         left: 0,
         behavior: 'smooth',
       });
     } catch (error) {
-      // just a fallback for older browsers
       window.scrollTo(0, 0);
     }
   }, [location]);
